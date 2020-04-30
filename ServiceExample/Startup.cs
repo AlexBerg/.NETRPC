ac51e8aea8ea30c33fe8d0781b5b1a3d4ad7c386
Alexander <bergersen.alexander@gmail.com>
2020-04-30 21:11:19 +0200

    - moved package dependency to RPC project instead

--- ServiceExample/Startup.cs
+++ ServiceExample/Startup.cs
@@ -2,7 +2,6 @@ using HelperLibrary;
 using HttpRPC.RPC.Client;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
